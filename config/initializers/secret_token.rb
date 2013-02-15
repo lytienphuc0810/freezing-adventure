a0a299b1944769952e55755c9250bdc751b34abf
Phuc Ly <lytienphuc0810@gmail.com>
2013-02-15 16:14:54 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FreezingAdventure::Application.config.secret_token = '4c3c6d64773f9386037993f856c293232b8b7ebde55d61c5d99ac7639c47b59400671e3635de2bede1b2cac768b9d031ecc336e59294d0264bc5523c1c0406f0'
