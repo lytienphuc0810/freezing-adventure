fbf593c38a8c1b0c0be9ef72826259e16d7e8686
Phuc Ly <lytienphuc0810@gmail.com>
2013-02-15 17:44:01 +0700

    add table

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,9 @@ FreezingAdventure::Application.routes.draw do
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  resources :tables do
+  end 
+
   # Sample resource route with options:
   #   resources :products do
   #     member do
